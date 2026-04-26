39fe427741f2d9f402250203da068b1a84f2ee49
Hidayat Tullah <ah3577470@gmail.com>
2026-04-27 06:40:16 +0700

    fix: add config variables

---
+++ config.py
@@ -0,0 +1,12 @@
+# =========================
+# CONFIG SYSTEM
+# =========================
+
+# AI
+WINDOW = 7
+
+# Threshold default (fallback kalau data belum cukup)
+THRESHOLD_DEFAULT = 500
+
+# Interval notif normal (detik)
+NOTIF_INTERVAL = 300  # 5 menit
